ea122a4931be465bae044434e1d496c22f582d17
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-20 12:59:31 -0800

    Added textarea to form.

--- app/views/shortcodes/wordio.php
+++ app/views/shortcodes/wordio.php
@@ -1,6 +1,9 @@
 <h2>Wordio Shortcode</h2>
 
 <form method="post">
+  <textarea name="wordio_text" rows="5">
+    Your Message Here.
+  </textarea>
   <input type="hidden" name="wordio_nonce" value="<?php echo wp_create_nonce('wordio-nonce');?>"/>
   <p class="submit">
     <input type="submit" class="button-primary" value="Send Text"/>
