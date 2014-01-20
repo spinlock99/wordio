13a53d84c7fc9085c11c88ef7c1c14026406725d
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-20 14:33:14 -0800

    Spell Twilio right to avoid errors.

--- admin/controllers/twillio.php
+++ admin/controllers/twillio.php
@@ -1,20 +1,20 @@
 <?php class TwillioAdminController {
 
   public function register_settings() {
-    register_setting('twillio_settings_group', 'twillio_settings');
+    register_setting('twilio_settings_group', 'twilio_settings');
   }
 
   public function menu_setup() {
     add_options_page(
-      'Twillio Settings', // $page_title
-      'Twillio Settings', // $menu_title
+      'Twilio Settings', // $page_title
+      'Twilio Settings', // $menu_title
       'manage_options',   // $capability
-      'twillio-settings', // $menu_slug
+      'twilio-settings', // $menu_slug
       array($this, 'render_options_page') //$function
     );
   }
 
   public function render_options_page() {
-    include(WORDIO_BASE_DIR . '/admin/views/twillio/options_page.php');
+    include(WORDIO_BASE_DIR . '/admin/views/twilio/options_page.php');
   }
 } ?>
