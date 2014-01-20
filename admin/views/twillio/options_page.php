c0e2fe0c2c931aa67c0ad64b7f7d8ba19b835a77
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-20 14:08:36 -0800

    Add field to admin table for Account SID.

--- admin/views/twillio/options_page.php
+++ admin/views/twillio/options_page.php
@@ -1,3 +1,11 @@
 <div class="wrap">
   <h2>Twillio Settings</h2>
+  <?php settings_fields('wordio_settings_group'); ?>
+  <table class="form-table">
+    <th>Account SID</th>
+    <td>
+    <input name="wordio_settings[account_sid]" type="text" class="regular-text" value="<?php
+      echo(isset($wordio_options[account_sid]) ? $wordio_options[account_sid] : '' );?>"/>
+    </td>
+  </table>
 </div>
