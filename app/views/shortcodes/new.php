4e9685ba3b87d9ec53692165f434f27593212c29
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-20 15:51:19 -0800

    Lightbox formatting.

--- app/views/shortcodes/new.php
+++ app/views/shortcodes/new.php
@@ -1,11 +1,19 @@
-<h2>Wordio Shortcode</h2>
-
-<form method="post">
-  <textarea name="wordio_text" rows="5">
-    Your Message Here.
-  </textarea>
-  <input type="hidden" name="wordio_nonce" value="<?php echo wp_create_nonce('wordio-nonce');?>"/>
-  <p class="submit">
-    <input type="submit" class="button-primary" value="Send Text"/>
-  </p>
-</form>
+<div class="dialog app">
+  <div class="panel">
+    <form method="post">
+      <article>
+        <textarea name="wordio_text" rows="5" placeholder="Your Message Here."></textarea>
+        <input
+          type  = "hidden"
+          name  = "wordio_nonce"
+          value = "<?php echo wp_create_nonce('wordio-nonce');?>"
+        />
+      </article>
+      <footer>
+        <p class="submit">
+          <input type="submit" class="button-primary" value="Send Text"/>
+        </p>
+      </footer>
+    </form>
+  </div>
+</div>
