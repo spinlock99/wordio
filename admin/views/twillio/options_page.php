43285086d18dd4134fb3a897cf1b3d1c05ff2b3e
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-20 14:25:54 -0800

    Refactor variable names.

--- admin/views/twillio/options_page.php
+++ admin/views/twillio/options_page.php
@@ -1,23 +1,23 @@
 <div class="wrap">
   <h2>Twillio Settings</h2>
   <form method="post" action="options.php">
-    <?php settings_fields('wordio_settings_group'); ?>
+    <?php settings_fields('twilio_settings_group'); ?>
     <table class="form-table">
       <tr><th>Account SID</th><td>
-      <input name="wordio_settings[account_sid]" type="text" class="regular-text" value="<?php
-        echo(isset($wordio_options['account_sid']) ? $wordio_options['account_sid'] : '' );?>"/>
+      <input name="twilio_settings[account_sid]" type="text" class="regular-text" value="<?php
+        echo(isset($twilio_options['account_sid']) ? $twilio_options['account_sid'] : '' );?>"/>
       </td></tr>
       <tr><th>Auth Token</th><td>
-      <input name="wordio_settings[auth_token]" type="text" class="regular-text" value="<?php
-        echo(isset($wordio_options['auth_token']) ? $wordio_options['auth_token'] : '');?>"/>
+      <input name="twilio_settings[auth_token]" type="text" class="regular-text" value="<?php
+        echo(isset($twilio_options['auth_token']) ? $twilio_options['auth_token'] : '');?>"/>
       </td></tr>
       <tr><th>From Number</th><td>
-      <input name="wordio_settings[from_number]" type="text" class="regular-text" value="<?php
-        echo(isset($wordio_options['from_number']) ? $wordio_options['from_number'] : '');?>"/>
+      <input name="twilio_settings[from_number]" type="text" class="regular-text" value="<?php
+        echo(isset($twilio_options['from_number']) ? $twilio_options['from_number'] : '');?>"/>
       </td></tr>
       <tr><th>To Number</th><td>
-      <input name="wordio_settings[to_number]" type="text" class="regular-text" value="<?php
-        echo(isset($wordio_options['to_number']) ? $wordio_options['to_number'] : '');?>"/>
+      <input name="twilio_settings[to_number]" type="text" class="regular-text" value="<?php
+        echo(isset($twilio_options['to_number']) ? $twilio_options['to_number'] : '');?>"/>
       </td></tr>
     </table>
     <p class="submit">
