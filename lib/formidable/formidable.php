46eacc4651bb4e958b93700167f0f78da1bc740b
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-21 12:16:49 -0800

    Drying out Formidable.

--- lib/formidable/formidable.php
+++ lib/formidable/formidable.php
@@ -2,18 +2,7 @@
   class Formidable {
     public function text($header, $name, $value, $label='') {
       ob_start();
-?>
-<tr>
-<th><?php echo $header; ?></th>
-  <td>
-  <input name="<?php echo $name; ?>" type="text" class="regular-text" value="<?php
-      echo(isset($value) ? $value : '');?>"/>
-    <label class="description" for="<?php echo $name; ?>">
-      <?php echo $label; ?>
-    </label>
-  </td>
-</tr>
-<?php
+      include 'views/text.php';
       return ob_get_clean();
     }
   }
