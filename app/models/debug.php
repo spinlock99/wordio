f2ae2a0053c4c5852f02dc8478ce43aaa51ebc35
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-21 10:22:29 -0800

    Add DebugModel with nice helper methods.

---
+++ app/models/debug.php
@@ -0,0 +1,28 @@
+<?php
+// Don't redeclare the Class if it already exists.
+if (!class_exists( 'DebugModel' )) {
+  class DebugModel{
+    public function log($message) {
+      if (WP_DEBUG_LOG === true) {
+        if (is_array($message) || is_object($message)) {
+          error_log(print_r($message, true));
+        } else {
+          error_log($message);
+        }
+      }
+    }
+
+    public function email($message) {
+      if (is_array($message) || is_object($message)) {
+        $body = print_r($message, true);
+      } else {
+        $body = $message
+      }
+      $to     = get_bloginfo('admin_email', 'raw');
+      $subjet = get_bloginfo('name', 'display') + " - Debug Message";
+
+      wp_mail($to, $subject, $message);
+    }
+  }
+}
+?>
