1e14a9dcd53306e9b65754a0039c9abeb991034b
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-21 11:34:27 -0800

    Refactor
    render_options_page() -> render_settings_page()

--- admin/controllers/settings_controller.php
+++ admin/controllers/settings_controller.php
@@ -14,11 +14,11 @@
       'Twilio Settings', // $menu_title
       'manage_options',   // $capability
       'twilio-settings', // $menu_slug
-      array($this, 'render_options_page') //$function
+      array($this, 'render_settings_page') //$function
     );
   }
 
-  public function render_options_page() {
+  public function render_settings_page() {
     $twilio_settings = get_option('twilio_settings');
     include(WORDIO_BASE_DIR . '/admin/views/settings/twilio.php');
   }
