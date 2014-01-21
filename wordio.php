fae56222bb80783d81e79b3f79369c83415f1cd2
Andrew Dixon <dixon@atomicbroadcast.net>
2014-01-21 09:08:14 -0800

    Take away separation of admin and user roles.

--- wordio.php
+++ wordio.php
@@ -16,14 +16,11 @@ if (!defined('WORDIO_BASE_DIR')) {
   define('WORDIO_BASE_DIR', dirname(__FILE__));
 }
 // Instantiate Controllers
-if (is_admin()) {
-  require_once(WORDIO_BASE_DIR . '/admin/controllers/twillio.php');
-  $twillioAdminController = new TwillioAdminController;
-  add_action('admin_init', array($twillioAdminController, 'register_settings'));
-  add_action('admin_menu', array($twillioAdminController, 'menu_setup'));
-} else {
-  require_once(WORDIO_BASE_DIR . '/app/controllers/wordio_shortcodes_controller.php');
-  $shortcodeController = new WordioShortcodesController;
-  require_once(WORDIO_BASE_DIR . '/lib/twilio-php-master/Services/Twilio.php');
-}
+require_once(WORDIO_BASE_DIR . '/admin/controllers/twillio.php');
+$twillioAdminController = new TwillioAdminController;
+add_action('admin_init', array($twillioAdminController, 'register_settings'));
+add_action('admin_menu', array($twillioAdminController, 'menu_setup'));
+require_once(WORDIO_BASE_DIR . '/app/controllers/wordio_shortcodes_controller.php');
+$shortcodeController = new WordioShortcodesController;
+require_once(WORDIO_BASE_DIR . '/lib/twilio-php-master/Services/Twilio.php');
 ?>
