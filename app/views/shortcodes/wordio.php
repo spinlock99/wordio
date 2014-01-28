1e5b8c8d8e3add3d8e2694f779b0d6464a7d2857
presspay <presspay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-28 19:32:27 +0000

    Added nonce to shortcode form.
    git-svn-id: http://plugins.svn.wordpress.org/wordio/trunk@847202 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- app/views/shortcodes/wordio.php
+++ app/views/shortcodes/wordio.php
@@ -1,6 +1,7 @@
 <h2>Wordio Shortcode</h2>
 
 <form method="post">
+  <input type="hidden" name="wordio_nonce" value="<?php echo wp_create_nonce('wordio-nonce');?>"/>
   <p class="submit">
     <input type="submit" class="button-primary" value="Send Text"/>
   </p>
